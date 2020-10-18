440d5343fdedff58c6853c91410726729179c7fd
Keith Saunders <keith.saunders682@gmail.com>
2020-10-19 00:35:58 +0300

    Dust Effect

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
+const DustEffect = preload("res://Scenes/Scenes/Effects/DustEffect.tscn")
+
 onready var sprite = $Sprite
 onready var spriteAnimator = $SpriteAnimator
+onready var coyoteJumpTimer = $CoyoteJumpTimer
 
 export (int) var ACCELERATION = 512
 export (int) var MAX_SPEED = 64
@@ -19,12 +22,19 @@ func _physics_process(delta: float) -> void:
 	var just_jumped = false
 	var input_vector = get_input_vector()
 	apply_horizontal_force(input_vector, delta)
-	apply_friction(input_vector)
-	apply_gravity(delta)
 	update_snap_vector()
+	apply_friction(input_vector)
 	jump_check()
+	apply_gravity(delta)
 	update_animation(input_vector)
 	move()
+
+func create_dust_effect():
+	var dust_position = global_position
+	dust_position.x += rand_range(-4, 4)
+	var dustEffect = DustEffect.instance()
+	get_tree().current_scene.add_child(dustEffect)
+	dustEffect.global_position = dust_position
 
 func get_input_vector():
 	var input_vector = Vector2.ZERO
@@ -42,10 +52,10 @@ func apply_friction(input_vector):
 		
 func update_snap_vector():
 	if is_on_floor():
-		snap_vector = Vector2.DOWN
+		snap_vector = -get_floor_normal()
 	
 func jump_check():
-	if is_on_floor():
+	if is_on_floor() or coyoteJumpTimer.time_left > 0:
 		if Input.is_action_just_pressed("ui_up"):
 			jump(JUMP_FORCE)
 			just_jumped = true
@@ -80,16 +90,19 @@ func move():
 
 	motion = move_and_slide_with_snap(motion, snap_vector * 4, Vector2.UP, true, 4,  deg2rad(MAX_SLOPE_ANGLE))
 	
-	# Checks on move and slide with snap	
+	# Checks on move and slide with snap
 	# Just left the ground
 	if was_on_floor and not is_on_floor() and not just_jumped:
 		motion.y = 0
 		position.y = last_position.y
+		coyoteJumpTimer.start()
 		
 	# Landing
 	if was_in_air and is_on_floor():
-		motion.x = last_motion.x	
+		motion.x = last_motion.x
+		create_dust_effect()
 	
 	# Prevent Sliding 
 	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) < 1:
 		position.x = last_position.x
+
