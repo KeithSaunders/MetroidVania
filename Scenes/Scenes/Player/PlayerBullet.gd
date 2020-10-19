21145b0e9055b8751ac57c50641d34f1cc001e36
Keith Saunders <keith.saunders682@gmail.com>
2020-10-19 23:00:06 +0300

    Player Bullet Animation Fix

---
+++ Scenes/Scenes/Player/PlayerBullet.gd
@@ -0,0 +1,9 @@
+extends "res://Scenes/Scenes/Player/Projectile.gd"
+
+
+# Removes the physics process from the bullet. It is renables on the bullet being
+# fired in the animation player. Prevents the bullet from moving before the muzzle
+# flare is called.
+func _ready() -> void:
+	set_process(false)
+
