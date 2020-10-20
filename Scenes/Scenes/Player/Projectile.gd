2a2a02273a4c892afbae9d243b12fa970ed26b1b
Keith Saunders <keith.saunders682@gmail.com>
2020-10-20 22:56:56 +0300

    Projectile class added signals and effect

--- Scenes/Scenes/Player/Projectile.gd
+++ Scenes/Scenes/Player/Projectile.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+const explosionEffect = preload("res://Scenes/Scenes/Effects/ExplosionEffect.tscn")
 
 var velocity = Vector2.ZERO
 
@@ -8,4 +9,16 @@ func _process(delta: float) -> void:
 
 
 func _on_VisibilityNotifier2D_viewport_exited(_viewport: Viewport) -> void:
+	queue_free()
+
+
+func _on_Hitbox_body_entered(body: Node) -> void:
+	Utils.instance_scene_on_main(explosionEffect, global_position)
+	queue_free()
+	
+
+
+
+func _on_Hitbox_area_entered(area: Area2D) -> void:
+	Utils.instance_scene_on_main(explosionEffect, global_position)
 	queue_free()
