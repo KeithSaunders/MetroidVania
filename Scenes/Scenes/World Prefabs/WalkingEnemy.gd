e007867c07faf5fe25f4d3272ca0dbc8f8580991
Keith Saunders <keith.saunders682@gmail.com>
2020-10-20 06:37:48 +0300

    Raycasting and Enum setup

--- Scenes/Scenes/World Prefabs/WalkingEnemy.gd
+++ Scenes/Scenes/World Prefabs/WalkingEnemy.gd
@@ -1,2 +1,11 @@
 extends "res://Scenes/Scenes/World Prefabs/Enemy.gd"
 
+# Enumerator to set up a dictionary. Going to be established as the left and
+# right direction
+enum DIRECTION {
+	LEFT = -1, 
+	RIGHT = 1
+	}
+
+# REMEMBER! Extends the max_speed, this will export the direction as well.
+export(DIRECTION) var WALKING_DIRECTION
