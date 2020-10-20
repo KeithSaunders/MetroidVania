00ae9f6465f2449aa77aea0cfd90cdc1cc07fdcd
Keith Saunders <keith.saunders682@gmail.com>
2020-10-20 23:21:30 +0300

    Update Enemy.gd
    Reference enemy stats and call down when receiving damage. Also receives signal from enemy stats when enemy has reached 0 health

--- Scenes/Scenes/World Prefabs/Enemy.gd
+++ Scenes/Scenes/World Prefabs/Enemy.gd
@@ -1,9 +1,15 @@
 extends KinematicBody2D
 
+onready var stats = $EnemyStats
+
 export (int) var MAX_SPEED = 15
 var motion = Vector2.ZERO
 
 # Triggers on hitbox colliding with hurtbox and signaling to the 
 # hutbox to take damage or 'hit'
 func _on_Hurtbox_hit(damage) -> void:
+	stats.health -= damage
+
+
+func _on_EnemyStats_enemy_died() -> void:
 	queue_free()
