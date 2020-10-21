2d66e0742cb885f06475b22b62e108aa9058e121
Keith Saunders <keith.saunders682@gmail.com>
2020-10-21 21:14:53 +0300

    Update StartMenu.gd

--- Scenes/Scenes/UI/Menus/StartMenu.gd
+++ Scenes/Scenes/UI/Menus/StartMenu.gd
@@ -8,9 +8,21 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	VisualServer.set_default_clear_color(Color.black)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+
+
+func _on_StartButton_pressed() -> void:
+	get_tree().change_scene("res://Scenes/Scenes/World.tscn")
+
+
+func _on_LoadButton_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_QuitButton_pressed() -> void:
+	get_tree().quit()
