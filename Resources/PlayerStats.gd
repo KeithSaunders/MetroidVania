ca53c097dd7021f41db3a539e248987e3e96f691
Keith Saunders <keith.saunders682@gmail.com>
2020-10-21 20:52:21 +0300

    Update PlayerStats.gd

--- Resources/PlayerStats.gd
+++ Resources/PlayerStats.gd
@@ -5,12 +5,13 @@ class_name PlayerStats
 var max_health = 4
 var health = max_health setget set_health
 
+signal player_health_update
 signal player_died
 
 func set_health(value):
-	
 	screenshake(value, .5, 0.5)
 	health = clamp(value, 0, max_health)
+	emit_signal("player_health_update", health)
 	if health == 0:
 		emit_signal("player_died")
 		
