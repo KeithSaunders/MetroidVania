1cf5c85a08481c2901d23ffd0f5388a3f71b0283
Keith Saunders <keith.saunders682@gmail.com>
2020-10-22 22:54:39 +0300

    Created Crawling Enemy

---
+++ Scenes/Scenes/World Prefabs/CrawlingEnemy.gd
@@ -0,0 +1,30 @@
+extends "res://Scenes/Scenes/World Prefabs/Enemy.gd"
+
+enum DIRECTION {
+	LEFT = -1,
+	RIGHT = 1
+}
+
+export (DIRECTION) var WALKING_DIRECTION = DIRECTION.RIGHT
+
+onready var floorCast = $FloorCast
+onready var wallCast = $WallCast
+onready var animate = $AnimationPlayer
+
+func _ready() -> void:
+	wallCast.cast_to.x = WALKING_DIRECTION
+	
+func _physics_process(delta: float) -> void:
+	animate.play("Animate")
+	if wallCast.is_colliding():
+		global_position = wallCast.get_collision_point()
+		var normal = wallCast.get_collision_normal()
+		rotation = normal.rotated(deg2rad(90)).angle()
+	else:
+		floorCast.rotation_degrees = -MAX_SPEED * 10 * WALKING_DIRECTION * delta
+		if floorCast.is_colliding():
+			global_position = floorCast.get_collision_point()
+			var normal = floorCast.get_collision_normal()
+			rotation = normal.rotated(deg2rad(90)).angle()
+		else:
+			rotation_degrees += 20 * WALKING_DIRECTION
