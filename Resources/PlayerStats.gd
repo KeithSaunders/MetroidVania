b1fdcdbcadbeaf2869a030bc7048a5aa16575a06
Keith Saunders <keith.saunders682@gmail.com>
2020-10-23 00:18:19 +0300

    Missles clamp

--- Resources/PlayerStats.gd
+++ Resources/PlayerStats.gd
@@ -3,10 +3,13 @@ extends Resource
 class_name PlayerStats
 
 var max_health = 4
+var max_missles = 3
+var missles = max_missles setget set_missles
 var health = max_health setget set_health
 
-signal player_health_update
+signal player_health_update(value)
 signal player_died
+signal player_missles_update(value)
 
 func set_health(value):
 	screenshake(value, .5, 0.5)
@@ -18,3 +21,7 @@ func set_health(value):
 func screenshake(healthValue, shakeStrength, duration):
 	if healthValue < health:
 		Events.emit_signal("add_screenshake", shakeStrength, duration)
+
+func set_missles(value):
+	missles = clamp(value, 0, max_missles)
+	emit_signal("player_missles_update", missles)
