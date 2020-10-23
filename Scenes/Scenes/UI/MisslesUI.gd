7b491b836fb97600c68afc54672b2c88d2449ad7
Keith Saunders <keith.saunders682@gmail.com>
2020-10-24 01:55:25 +0300

    Hidden Missile UI and Signals

--- Scenes/Scenes/UI/MisslesUI.gd
+++ Scenes/Scenes/UI/MisslesUI.gd
@@ -5,7 +5,12 @@ var playerStats = ResourceLoader.PlayerStats
 onready var missile_label = $MissileLabel
 
 func _ready() -> void:
+#self.set_visible(false)
 	playerStats.connect("player_missiles_update", self, "_on_player_missiles_update")
+	playerStats.connect("player_missiles_unlocked", self, "_on_player_missiles_unlocked")
 	
 func _on_player_missiles_update(missiles):
 	missile_label.text = str(missiles)
+	
+func _on_player_missiles_unlocked(value):
+	self.visible = value
