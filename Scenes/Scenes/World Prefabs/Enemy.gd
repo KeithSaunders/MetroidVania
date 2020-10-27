205d84270ef3b022291b19ea0629fb3d7be03c04
Keith Saunders <keith.saunders682@gmail.com>
2020-10-27 21:37:06 +0300

    Update Enemy.gd

--- Scenes/Scenes/World Prefabs/Enemy.gd
+++ Scenes/Scenes/World Prefabs/Enemy.gd
@@ -3,8 +3,11 @@ extends KinematicBody2D
 onready var stats = $EnemyStats
 
 const EnemyDeathEffect = preload("res://Scenes/Scenes/Effects/EnemyDeathEffect.tscn")
+const HealthPickUp = preload("res://Scenes/Scenes/Player/HealthPickup.tscn")
 
 export (int) var MAX_SPEED = 15
+export (int) var DROP_CHANCE = 5
+
 var motion = Vector2.ZERO
 
 # Triggers on hitbox colliding with hurtbox and signaling to the 
@@ -15,6 +18,13 @@ func _on_Hurtbox_hit(damage) -> void:
 
 func _on_EnemyStats_enemy_died() -> void:
 	queue_free()
+	dropHealth(DROP_CHANCE)
 	Utils.instance_scene_on_main(EnemyDeathEffect, global_position)
+	
+func dropHealth(chance):
+	var chance_to_drop = rand_range(0, 100)
+	if chance_to_drop <= chance:
+		Utils.instance_scene_on_main(HealthPickUp, global_position)
+	
 	
 	
