21cbf5f89db5caa79be3627b3715a0814eb6db36
Keith Saunders <keith.saunders682@gmail.com>
2020-10-27 21:37:13 +0300

    Update Level.gd

--- Scenes/Scenes/Levels/Level.gd
+++ Scenes/Scenes/Levels/Level.gd
@@ -1,8 +1,24 @@
 extends Node2D
 
-const WORLD = preload("res://Scenes/Scenes/World.gd")
+
+const PlayerCinematic = preload("res://Scenes/Scenes/Player/PlayerCinematic.tscn")
+
+
+onready var animationPlayer = $AnimationPlayer
+
 
 func _ready() -> void:
-	var parent = get_parent()
-	if parent is WORLD:
-		parent.current_level = self
+	VisualServer.set_default_clear_color(Color.black)
+	
+
+	
+func play_cinematic():
+	animationPlayer.play("Animate")
+	
+	
+func change_scene():
+	get_tree().change_scene("res://Scenes/Scenes/World.tscn")
+
+
+func _on_Level_Intro_ready() -> void:
+	play_cinematic()
