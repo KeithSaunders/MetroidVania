3b64217eaddda3b4f4aa64be01c3bb4671b65644
Keith Saunders <keith.saunders682@gmail.com>
2020-10-28 23:41:22 +0300

    Update StartMenu.gd

--- Scenes/Scenes/UI/Menus/StartMenu.gd
+++ Scenes/Scenes/UI/Menus/StartMenu.gd
@@ -1,24 +1,17 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	VisualServer.set_default_clear_color(Color.black)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+
 
 
 func _on_StartButton_pressed() -> void:
-	#get_tree().change_scene("res://Scenes/Scenes/Levels/Level_Intro.tscn")
-	get_tree().change_scene("res://Scenes/Scenes/World.tscn")
+# warning-ignore-all:return_value_discarded
+	get_tree().change_scene("res://Scenes/Scenes/Levels/Level_Intro.tscn")
 
 func _on_LoadButton_pressed() -> void:
 	SaveandLoader.is_loading = true
