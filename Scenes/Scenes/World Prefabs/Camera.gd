4594f7ede44a61ec05e87d0d26361325c7368efe
Keith Saunders <keith.saunders682@gmail.com>
2020-10-28 23:41:24 +0300

    Update Camera.gd

--- Scenes/Scenes/World Prefabs/Camera.gd
+++ Scenes/Scenes/World Prefabs/Camera.gd
@@ -6,6 +6,7 @@ var shake = 0
 onready var timer = $Timer
 
 func _ready() -> void:
+	# warning-ignore:return_value_discarded
 	Events.connect("add_screenshake", self, "_on_Events_add_screenshake")
 	mainInstance.WorldCamera = self
 	
@@ -16,7 +17,7 @@ func queue_free():
 
 # Random offset per frame on horizontal and vertical for the camera
 # Default is 0
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	offset_h = rand_range(-shake, shake)
 	offset_v = rand_range(-shake, shake)
 
