cc2acfbc425c8c8d0a3c9c5e62e81cf929baf903
Keith Saunders <keith.saunders682@gmail.com>
2020-10-28 21:50:10 +0300

    Savestation script created

---
+++ Scenes/Scenes/World Prefabs/SaveStation.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+
+onready var animationPlayer = $AnimationPlayer
+
+func _on_SaveArea_body_entered(body: Node) -> void:
+	animationPlayer.play("Animate")
+	SaveandLoader.save_game()
