b4f6cbb69f2ce798fb7e89eb9890eef6cc12cf76
Keith Saunders <keith.saunders682@gmail.com>
2020-10-28 23:13:03 +0300

    Print statements

--- Scenes/Scenes/World Prefabs/SaveStation.gd
+++ Scenes/Scenes/World Prefabs/SaveStation.gd
@@ -6,3 +6,5 @@ onready var animationPlayer = $AnimationPlayer
 func _on_SaveArea_body_entered(body: Node) -> void:
 	animationPlayer.play("Animate")
 	SaveandLoader.save_game()
+	print("Saved")
+	print(body)
