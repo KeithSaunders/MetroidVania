1d1ef1b213381dab061256f8dd1e49614f56a9c4
Keith Saunders <keith.saunders682@gmail.com>
2020-10-28 23:11:57 +0300

    Fixed Level and added save

--- Scenes/Scenes/Levels/Level.gd
+++ Scenes/Scenes/Levels/Level.gd
@@ -1,8 +1,25 @@
 extends Node2D
 
+
 const WORLD = preload("res://Scenes/Scenes/World.gd")
 
 func _ready():
 	var parent = get_parent()
 	if parent is WORLD:
 		parent.current_level = self
+
+		
+func save():
+	# Create a save_dictioary that gets the following information
+	# 1. File name with get_filename()
+	# 2. Gets the parent and path, to add as a child later.
+	# 3. Gets the position x
+	# 4. Gets the position y
+	var save_dictionary = {
+		"filename" : get_filename(),
+		"parent" : get_parent().get_path(),
+		"position_x" : position.x,
+		"position_y" : position.y
+	}
+	print("got here")
+	return save_dictionary
