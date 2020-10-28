1dafae1b6b9ae0782013a31f99efd6d087bbbb3f
Keith Saunders <keith.saunders682@gmail.com>
2020-10-28 23:41:11 +0300

    Update Events.gd

--- Resources/Events.gd
+++ Resources/Events.gd
@@ -3,6 +3,7 @@ extends Node
 # Handles events | Singleton to be called
 
 # Signal for adding screenshake to the camera
+# warning-ignore-all:unused_signal
 signal add_screenshake(amount, duration)
 
 signal movement_instructions(triggered)
