db0bafe92d78316108fd1feebff9ab3b7e6551af
Keith Saunders <keith.saunders682@gmail.com>
2020-10-28 21:03:17 +0300

    SaveandLoader Json Fix

--- Scripts/SaveandLoader.gd
+++ Scripts/SaveandLoader.gd
@@ -28,17 +28,19 @@ func load_game():
 		
 	save_game.open("user://savegame.save", File.READ)
 	while not save_game.eof_reached():
-		var current_line = parse_json(save_game.get_line())
-		if current_line != null:
-			var newNode = load(current_line["filename"]).instance()
-			get_node(current_line["parent"]).add_child(newNode, true)
-			newNode.position = Vector2(current_line["position_x"], current_line["position_y"])
-			for property in current_line.keys():
-				if (property == "filename" 
-				or property == "parent"
-				or property == "position_x"
-				or property == "position_y"):
-					continue
-				newNode.set(property, current_line[property])
+		var line_string = save_game().get_line()
+		if line_string != "":
+			var current_line = parse_json(line_string)
+			if current_line != null:
+				var newNode = load(current_line["filename"]).instance()
+				get_node(current_line["parent"]).add_child(newNode, true)
+				newNode.position = Vector2(current_line["position_x"], current_line["position_y"])
+				for property in current_line.keys():
+					if (property == "filename" 
+					or property == "parent"
+					or property == "position_x"
+					or property == "position_y"):
+						continue
+					newNode.set(property, current_line[property])
 	save_game.close()
 				
