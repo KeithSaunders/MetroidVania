eeee14c2550ca0bab9a98df8c220850116b72df5
Keith Saunders <keith.saunders682@gmail.com>
2020-10-28 23:41:19 +0300

    Update PauseMenu.gd

--- Scenes/Scenes/UI/Menus/PauseMenu.gd
+++ Scenes/Scenes/UI/Menus/PauseMenu.gd
@@ -8,7 +8,7 @@ func set_paused(value):
 	get_tree().paused = paused
 	visible = paused
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		self.paused = !paused
 
