ddc19963f2823be90ff4c14f416a6b35fbcdebe9
Keith Saunders <keith.saunders682@gmail.com>
2020-10-28 21:50:17 +0300

    Update SaveandLoader.gd

--- Scripts/SaveandLoader.gd
+++ Scripts/SaveandLoader.gd
@@ -28,19 +28,29 @@ func load_game():
 		
 	save_game.open("user://savegame.save", File.READ)
 	while not save_game.eof_reached():
+		# Gets the line and places in the var line_string
 		var line_string = save_game().get_line()
+		# Check if the line is blank or empty string
 		if line_string != "":
+			# parse the json string and places in current_line
 			var current_line = parse_json(line_string)
+			# If the current_line is not null
 			if current_line != null:
+				# loads the filename node and instances in the newNode
 				var newNode = load(current_line["filename"]).instance()
+				# Gets the parent node and adds the child
 				get_node(current_line["parent"]).add_child(newNode, true)
+				# Sets the newNode position
 				newNode.position = Vector2(current_line["position_x"], current_line["position_y"])
+				# Checks each key and checks the properties for the following properties
 				for property in current_line.keys():
 					if (property == "filename" 
 					or property == "parent"
 					or property == "position_x"
 					or property == "position_y"):
 						continue
+					# Sets the property and the current_line[property] key and value
 					newNode.set(property, current_line[property])
+	# Closes the save game
 	save_game.close()
 				
