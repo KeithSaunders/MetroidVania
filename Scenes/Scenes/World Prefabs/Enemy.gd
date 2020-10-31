45f0fea6936e447b6f4713b1cc390b0e0f35e691
Keith Saunders <keith.saunders682@gmail.com>
2020-10-31 17:17:41 +0300

    Modify Drop Chance for Health

--- Scenes/Scenes/World Prefabs/Enemy.gd
+++ Scenes/Scenes/World Prefabs/Enemy.gd
@@ -5,8 +5,10 @@ onready var stats = $EnemyStats
 const EnemyDeathEffect = preload("res://Scenes/Scenes/Effects/EnemyDeathEffect.tscn")
 const HealthPickUp = preload("res://Scenes/Scenes/World Prefabs/ItemDrop/HealthDrop.tscn")
 
+onready var playerStats = ResourceLoader.PlayerStats
+
 export (int) var MAX_SPEED = 15
-export (int) var DROP_CHANCE = 100
+
 
 var motion = Vector2.ZERO
 
@@ -19,13 +21,21 @@ func _on_Hurtbox_hit(damage) -> void:
 func _on_EnemyStats_enemy_died() -> void:
 	queue_free()
 	SoundFx.play("EnemyDie")
-	dropHealth(DROP_CHANCE)
-	Utils.instance_scene_on_main(EnemyDeathEffect, global_position + Vector2(0,-5))
+	var chance = dropChance()
+	dropHealth(chance)
+	Utils.instance_scene_on_main(EnemyDeathEffect, global_position + Vector2(0,-8))
 	
 func dropHealth(chance):
 	var chance_to_drop = rand_range(0, 100)
 	if chance_to_drop <= chance:
 		Utils.instance_scene_on_main(HealthPickUp, global_position)
 	
-	
-	
+func dropChance():
+	var chance = 0
+	if playerStats.health == playerStats.max_health:
+		chance = 0
+	if playerStats.health < playerStats.max_health:
+		chance = (playerStats.health / playerStats.max_health) - 20
+		if chance < 0:
+			chance = 10
+	return chance
