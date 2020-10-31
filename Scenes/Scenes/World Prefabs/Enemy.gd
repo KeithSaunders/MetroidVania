fe1fdcd300455524a6e81dfbc429a2df62440e63
Keith Saunders <keith.saunders682@gmail.com>
2020-10-31 22:28:45 +0300

    Typo

--- Scenes/Scenes/World Prefabs/Enemy.gd
+++ Scenes/Scenes/World Prefabs/Enemy.gd
@@ -13,7 +13,7 @@ export (int) var MAX_SPEED = 15
 var motion = Vector2.ZERO
 
 # Triggers on hitbox colliding with hurtbox and signaling to the 
-# hutbox to take damage or 'hit'
+# hurtbox to take damage or 'hit'
 func _on_Hurtbox_hit(damage) -> void:
 	stats.health -= damage
 
