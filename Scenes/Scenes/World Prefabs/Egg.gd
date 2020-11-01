cc489ece6a316c584c3c6c0e5b87d4e2cd3765dc
Keith Saunders <keith.saunders682@gmail.com>
2020-11-01 23:59:26 +0300

    Update Egg.gd

--- Scenes/Scenes/World Prefabs/Egg.gd
+++ Scenes/Scenes/World Prefabs/Egg.gd
@@ -1,6 +1,8 @@
 extends "res://Scenes/Scenes/World Prefabs/Enemy.gd"
 
+onready var animationPlayer = $AnimationPlayer
 
+const harpy = preload("res://Scenes/Scenes/World Prefabs/Harpy.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,7 +14,14 @@ func _on_EnemyStats_enemy_died() -> void:
 	# Call Parent function
 	._on_EnemyStats_enemy_died()
 
+func spawn_from_hatch():
+	Utils.instance_scene_on_main(harpy, global_position)
+	
+
+
 func _on_VisibilityNotifier2D_screen_entered() -> void:
 	# Begin animation for hatching
-	
-	pass # Replace with function body.
+	animationPlayer.play("Shake")
+
+func hatch():
+	animationPlayer.play("Hatch")
