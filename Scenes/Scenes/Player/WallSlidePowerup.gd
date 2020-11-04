48a4fd3e1bbc014b1d047a57af318d3d2fb5bdea
Keith Saunders <keith.saunders682@gmail.com>
2020-11-04 22:42:21 +0300

    Create WallSlidePowerup.gd

---
+++ Scenes/Scenes/Player/WallSlidePowerup.gd
@@ -0,0 +1,16 @@
+extends Powerup
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _pickup():
+	playerStats.wall_slide_unlocked = true
+	._pickup()
